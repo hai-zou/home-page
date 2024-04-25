d7dccfabe5cdbad6e16fa57f413111d0c6cc1976
zouhai <zou.hai@outlook.com>
2024-04-25 22:25:28 +0800

    feat: create random place

--- app/page.tsx
+++ app/page.tsx
@@ -25,58 +25,67 @@ export default function Home() {
     ))
   };
 
+  const createAllGrid = () => {
+    const blockList = createGridItem(75);
+    const customList = [
+      <div key='OneWord' className='grid-item' style={{ width: '430px' }}>
+        <OneWord loadComplete={() => oneWordLoaded()} />
+      </div>,
+      <div key='Learn' className='grid-item' style={{ width: '320px' }}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+      </div>,
+      <div key='Templates' className='grid-item' style={{ width: '210px' }}>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+      </div>,
+      <div key='Deploy' className='grid-item' style={{ width: '320px' }}>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    ];
+    while (customList.length) {
+      const item = customList.shift();
+      if (!item) return;
+      const randomIndex = Math.floor(Math.random() * (blockList.length + 1));
+      blockList.splice(randomIndex, 0, item);
+    }
+    return blockList;
+  };
+
   return (
     <main className={styles.main}>
       <PackeryLayout isReset={isReset}>
-        {createGridItem(15)}
-        <div className='grid-item' style={{ width: '430px' }}>
-          <OneWord loadComplete={() => oneWordLoaded()} />
-        </div>
-        {createGridItem(15)}
-        <div className='grid-item' style={{ width: '320px' }}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-          </a>
-        </div>
-        {createGridItem(15)}
-        <div className='grid-item' style={{ width: '210px' }}>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>Explore starter templates for Next.js.</p>
-          </a>
-        </div>
-        {createGridItem(15)}
-        <div className='grid-item' style={{ width: '320px' }}>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div>
-        {createGridItem(15)}
+        {createAllGrid()}
       </PackeryLayout>
     </main>
   );
