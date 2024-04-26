6ec18f855010d78cb72f0e448050a44fe83da046
zouhai <zou.hai@outlook.com>
2024-04-26 11:47:04 +0800

    fix: update api url

--- api-base-url.ts
+++ api-base-url.ts
@@ -1,6 +1,11 @@
+/**
+ * /imgapi -> https://imgapi.cn
+ * /hitokoto -> https://v1.hitokoto.cn
+ */
+
 const BASE_URL = process.env.NODE_ENV === 'production' ?
 {
-  "IMG_API": 'https://imgapi.cn',
+  "IMG_API": '/imgapi',
   "HITOKOTO_API": 'https://v1.hitokoto.cn',
 }: {
   "IMG_API": '/imgapi',
