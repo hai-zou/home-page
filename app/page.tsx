9046094a6397078335d0202313593417e5da4421
zouhai <zou.hai@outlook.com>
2024-04-28 18:01:48 +0800

    feat: 接入 Umami 站点分析

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import Carousel from "./carousel";
 import LoadingPage from "./loading-page";
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 export default function Home() {
   const [isPageLoading, setIsPageLoading] = useState(true);
@@ -23,6 +24,13 @@ export default function Home() {
       <main className={styles.main}>
         <Carousel />
       </main>
+      {process.env.NODE_ENV === 'production' ? (
+        <Script
+          defer
+          src="https://analytics.luckyzh.cn/script.js"
+          data-website-id="2f9b3ca5-27e8-47e4-a618-1a225b7d99fd"
+        />
+      ) : null}
     </>
   );
 }
