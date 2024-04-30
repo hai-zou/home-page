b3de73a73d4d0ece9b7786b53ade7a0d66970db7
zouhai <zou.hai@outlook.com>
2024-04-30 15:45:06 +0800

    chore: remove packery component

--- components/packery/packery-ref.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-import { forwardRef } from 'react';
-
-const PackeryLayout = dynamic(() => import('./index'), { ssr: false });
-const ForwardedRefPackery = forwardRef(function ForwardedRefPackery(props: { children: React.ReactNode; }, ref) {
-  return <PackeryLayout forwardedRef={ref}>{props.children}</PackeryLayout>
-});
-
-export default ForwardedRefPackery
