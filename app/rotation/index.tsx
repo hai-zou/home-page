9654c795c6a639e76a854ce42d8cabf79d7dbcdd
zouhai <zou.hai@outlook.com>
2024-05-08 09:14:27 +0800

    feat: card sort

--- app/rotation/index.tsx
+++ app/rotation/index.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { displayList } from '@/app/data';
-import CardItem from '@/app/cards';
+import CardItem, { AllCard } from '@/app/cards';
 import styles from './index.module.css';
 import useMouseMove from './mouse-move';
 import useTouchMove from './touch-move';
@@ -12,16 +12,25 @@ const isMobileDevice = () => {
   return (typeof window.ontouchstart !== 'undefined');
 }
 
+type CardOrder = {
+  [K in AllCard['type']]: number;
+}
+
+// 卡片排序
+const cardOrder: CardOrder = { user: 1, project: 2, custom: 3 };
+const rotateList = [...displayList];
+rotateList.sort((a, b) => cardOrder[a.type] - cardOrder[b.type]);
+
 const Rotation = () => {
 
   const wrapRef = useRef<HTMLDivElement>(null);
 
   // 半径
-  const radius = 60 * displayList.length;
+  const radius = 60 * rotateList.length;
 
   const getCardStyle = (index: number) => {
     // 旋转角度
-    const rotateDeg = index * (360 / displayList.length);
+    const rotateDeg = index * (360 / rotateList.length);
     // 动画时间
     const animateTime = index * 0.1;
     return {
@@ -40,7 +49,7 @@ const Rotation = () => {
     <div id={styles.perspective}>
       <div className={styles['center-axis']} style={{ transform: `translateZ(${-radius}px)` }}>
         <div className={styles.wrap} ref={wrapRef}>
-          {displayList.map((item, index) => (
+          {rotateList.map((item, index) => (
             <div
               key={item.id}
               className={styles['card']}
